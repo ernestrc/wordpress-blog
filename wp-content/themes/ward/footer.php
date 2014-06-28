7acf0872f7da02cf810e789b81a2212cc7a56e4e
Ernest <lordnesi28@gmail.com>
2014-06-28 13:37:22 +0100

    Added outdated browser plugin

--- wp-content/themes/ward/footer.php
+++ wp-content/themes/ward/footer.php
@@ -37,5 +37,10 @@ $bavotasan_theme_options = bavotasan_theme_options();
 </footer><!-- #footer -->
 
 <?php wp_footer(); ?>
+<div id="outdated">
+    <h6>Your browser is out-of-date!</h6>
+    <p>Update your browser to view this website correctly. <a id="btnUpdateBrowser" href="http://outdatedbrowser.com/">Update my browser now </a></p>
+    <p class="last"><a href="#" id="btnCloseUpdateBrowser" title="Close">&times;</a></p>
+</div>
 </body>
 </html>
