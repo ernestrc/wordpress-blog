6ee6ae28cc4ffe3155f36c0dcb5163c10d15fb26
Ernest <lordnesi28@gmail.com>
2014-07-19 20:48:29 +0100

    Fixed bugs in text-shadow submenu, and changed glow intensity of fa-bolt

--- wp-content/themes/ward/library/js/theme.js
+++ wp-content/themes/ward/library/js/theme.js
@@ -923,7 +923,7 @@
     function bolt(item,start){
         setTimeout(function(){
             if(start % 2) {
-                jQuery(item).css('text-shadow','0 0 10px #FFF')
+                jQuery(item).css('text-shadow','0 0 5px #FFF')
             } else {
                 jQuery(item).css('text-shadow',"0 1px 2px rgba(0, 0, 0, 0.6)")
             }
